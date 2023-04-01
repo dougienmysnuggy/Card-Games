6512f56e6bbf8c05f5fada1adbb0dd9eca741541
dougienmysnuggy <leonardw@gmail.com>
2023-04-01 00:07:34 -0400

    working on base classes for deck of cards

---
+++ cards.py
@@ -0,0 +1,24 @@
+class Card:
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+
+class Deck:
+    def __init__(self):
+        card-deck = []
+        for i in ['hearts', 'spades', 'clubs', 'diamonds']:
+            for j in range(1-14):
+                card-deck.push()
+
+
+    def shuffle():
+        pass
+
+
+    def deal():
+        pass
+
+
+    def draw():
+        pass
