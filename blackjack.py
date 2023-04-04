224bee62288f9636593475f6763862c3c8dcba62
dougienmysnuggy <leonardw@gmail.com>
2023-04-04 13:19:41 -0400

    added main function

--- blackjack.py
+++ blackjack.py
@@ -14,33 +14,44 @@ class Player():
         self.player_name = name
         self.player_money = bankroll
         
-# Initialize round                
-blackjack_deck = cards.Deck()
-dealer = Player("DEALER", 9999999)
-player1 = Player("PLAYER 1", "5000")
-blackjack_card = cards.Card("","")
 
-# Main game loop 
-while True:
-    blackjack_deck.reset_deck()
-    blackjack_deck.shuffle()
-    dealer_hand, player1_hand = [], []
 
-    # Need to accept bets
-    
-    # Deal initial hand
-    player1_hand.append(blackjack_deck.deal_card())
-    dealer_hand.append(blackjack_deck.deal_card())
-    player1_hand.append(blackjack_deck.deal_card())
-    dealer_hand.append(blackjack_deck.deal_card())
-    for blackjack_card in player1_hand:
-        blackjack_card.print_card()
+def main():
+    # Initialize round                
+    blackjack_deck = cards.Deck()
+    dealer = Player("DEALER", 9999999)
+    player1 = Player("PLAYER 1", "5000")
+    blackjack_card = cards.Card("","")
 
-    for blackjack_card in dealer_hand:
-        blackjack_card.print_card()
+
+
+    # Main game loop 
+
+    while True:
+        blackjack_deck.reset_deck()
+        blackjack_deck.shuffle()
+        dealer_hand, player1_hand = [], []
+
+        # Need to accept bets
         
-# This is currently an infinite loop that just keeps dealing 2 cards to dealer and player 1
+        
+        # Deal initial hand
+        player1_hand.append(blackjack_deck.deal_card())
+        dealer_hand.append(blackjack_deck.deal_card())
+        player1_hand.append(blackjack_deck.deal_card())
+        dealer_hand.append(blackjack_deck.deal_card())
+        
+        # Testing to print out player and dealer hands. Need to find a 
+        # better way to display the hands on screen.
+        for blackjack_card in player1_hand:
+            blackjack_card.print_card()
+
+        for blackjack_card in dealer_hand:
+            blackjack_card.print_card()
+            
+    # This is currently an infinite loop that just keeps dealing 2 cards to dealer and player 1
 
 
-
+if __name__ == "__main__":
+    main()
 
